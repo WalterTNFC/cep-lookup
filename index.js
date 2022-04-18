b1e7f936418dd2cff8af66ab326680c2adfd3626
WalterTNFC <walter.tad@hotmail.com>
2022-04-18 16:34:42 -0300

    Criado o EndPoint get com a rota /ping

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // carregando as variáveis de ambiente
 require('dotenv').config();
 const express = require('express');
+const res = require('express/lib/response');
+
+// Iniciando uma nova aplicação
+const app = express()
+
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'pong!' });
+});
 
