85ae3a96ac8b4524ae151625bb8489d640f812a6
WalterTNFC <walter.tad@hotmail.com>
2022-04-18 16:40:42 -0300

    Criada PORT 3000 no Index e adicionado start ao package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const res = require('express/lib/response');
+const PORT = 3000;
 
 // Iniciando uma nova aplicação
 const app = express()
@@ -10,3 +11,4 @@ app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong!' });
 });
 
+app.listen(PORT, () => { console.log(`Ouvindo a porta ${PORT}`); });
