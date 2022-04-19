fd57b2ac3a842d053a5f4fd0088012c5f7f37e25
WalterTNFC <walter.tad@hotmail.com>
2022-04-18 22:04:22 -0300

    Iniciada camada de servicos

---
+++ services/Cep.js
@@ -0,0 +1,8 @@
+const Cep = require('../models/Cep');
+
+const CEP_REGEX = /\d{5}-?\d{3}/;
+
+async function findAddressByCep(shearchedCep) {
+  // Valida o CEP e em caso dele ser falso, retorna um erro
+  
+}
